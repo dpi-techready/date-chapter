0d926a8cfd3ccb18797836847ef55c872878de74
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-09-28 02:51:48 +0000

    Update date_math.rb

--- date_math.rb
+++ date_math.rb
@@ -5,3 +5,10 @@
 # Output:
 #   "Ruby is 108937 days old!"
 #
+# date_sent = Date.parse(“2007-01-01”)
+date_sent = Date.new(“2007-01-01”)
+date_now = Date.today
+
+days_passed = date_now - date_sent
+
+puts "Ruby is " + days_passed + "days old!"
